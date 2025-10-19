ed5a7bd0a5e4621ebec8ac9665f231379fe06e9b
bisekhkarki <karkidai79@gmail.com>
2025-10-19 22:24:38 +0545

    Voting App backend api completed

---
+++ votingapp/src/main/java/com/voting/votingapp/VotingappApplication.java
@@ -0,0 +1,16 @@
+package com.voting.votingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// 3:41
+
+@SpringBootApplication
+public class VotingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingappApplication.class, args);
+	}
+
+}
